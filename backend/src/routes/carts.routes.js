e213d21c7a24e2e2c56319c236b9b1a915946a7e
martinlpc <martinlpacheco@gmail.com>
2023-06-12 17:37:12 -0300

    Initial commit

---
+++ backend/src/routes/carts.routes.js
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import { purchaseCart, getCart, addProduct, changeProductQuantity, clearCart, removeProduct, overwriteCart, createNewCart } from "../controllers/cart.controller.js";
+import { Roles, checkRole } from "../middlewares/session.js";
+
+const routerCart = Router()
+
+// Middleware to use in every cart related request
+routerCart.use(checkRole(Roles.USER))
+
+routerCart.route('/')
+    .post(createNewCart)
+    .get(getCart)
+    .put(overwriteCart)
+
+routerCart.route('/:cid')
+    .delete(clearCart)
+
+routerCart.route('/product/:pid')
+    .post(addProduct)
+
+routerCart.route('/:cid/product/:pid')
+    .put(changeProductQuantity)
+    .delete(removeProduct)
+
+routerCart.route('/purchase')
+    .post(purchaseCart)
+
+export default routerCart
---
+++ backend/src/utils/errors/enums.js
@@ -0,0 +1,8 @@
+export const EErrors = {
+    REQUIRED_ERROR: 1,
+    INVALID_TYPES_ERROR: 2,
+    DATABASE_ERROR: 3,
+    UNHANDLED_ERROR: 4,
+    PRODUCT_ERROR: 5
+}
+
