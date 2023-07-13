74bd50e36b1a3c802d075d89c100a23834c44b34
martinlpc <martinlpacheco@gmail.com>
2023-07-13 18:58:59 -0300

    Add invoice management to cart context

--- frontend/src/context/CartContext.jsx
+++ frontend/src/context/CartContext.jsx
@@ -8,44 +8,46 @@ export const CartProvider = ({ children }) => {
     // * newCart es el resultado de modificar el cart
     // * --------------------------------------------
     const [cart, setCart] = useState([]);
-    const [orderId, setOrderId] = useState(null);
     const [totalItems, setTotalItems] = useState(0);
     const [totalPrice, setTotalPrice] = useState(0);
+    const [ticket, setTicket] = useState(null);
 
     useEffect(() => {
-        console.log('Cart entrando en effect: ', cart);
         setTotalItems(cart.reduce((previous, current) => previous + current.quantity, 0));
         setTotalPrice(cart.reduce((previous, current) => previous + current.quantity * current.price, 0));
     }, [cart]);
 
     useEffect(() => {
-        const fetchCart = async () => {
-            try {
-                const fetchedCart = await getCart();
-                console.log('fetchedCart: ', fetchedCart.cart);
-
-                if (Array.isArray(fetchedCart.cart.products)) {
-                    const newCart = [];
-
-                    for (const product of fetchedCart.cart.products) {
-                        const newObject = {
-                            ...product.productId,
-                            quantity: product.quantity,
-                        };
-                        newCart.push(newObject);
-                    }
-
-                    setCart(newCart);
-                }
-            } catch (error) {
-                console.error('Error fetching cart:', error);
-            }
-        };
-
         fetchCart();
     }, []);
 
-    const addToCart = async (item, qty) => {
+    useEffect(() => {
+        fetchCart();
+    }, [ticket]);
+
+    async function fetchCart() {
+        try {
+            const fetchedCart = await getCart();
+
+            if (Array.isArray(fetchedCart.cart.products)) {
+                const newCart = [];
+
+                for (const product of fetchedCart.cart.products) {
+                    const newObject = {
+                        ...product.productId,
+                        quantity: product.quantity,
+                    };
+                    newCart.push(newObject);
+                }
+
+                setCart(newCart);
+            }
+        } catch (error) {
+            console.error('Error fetching cart:', error);
+        }
+    }
+
+    async function addToCart(item, qty) {
         if (isInCart(item._id)) {
             // * Local management
             let index = cart.findIndex((elem) => elem._id === item._id);
@@ -65,7 +67,6 @@ export const CartProvider = ({ children }) => {
         } else {
             // * Local management
             setCart([...cart, { ...item, quantity: qty }]);
-
             // * Remote management
             try {
                 await addSingleProductToCart(item._id);
@@ -76,9 +77,9 @@ export const CartProvider = ({ children }) => {
                 console.error('Error adding to the cart: ', error);
             }
         }
-    };
+    }
 
-    const removeItemById = async (id) => {
+    async function removeItemById(id) {
         // Elimina el producto y el total de sus unidades
         const newCart = [...cart];
         let index = newCart.findIndex((elem) => elem._id === id);
@@ -90,9 +91,9 @@ export const CartProvider = ({ children }) => {
         } catch (error) {
             console.error('Error removing from cart:', error);
         }
-    };
+    }
 
-    const clearCart = async () => {
+    async function clearCart() {
         setCart([]);
 
         try {
@@ -100,16 +101,28 @@ export const CartProvider = ({ children }) => {
         } catch (error) {
             console.error('Error clearing cart:', error);
         }
-    };
+    }
 
     const isInCart = (productID) => {
         return cart.some((item) => item._id === productID);
     };
 
+    async function buyCart() {
+        try {
+            const purchaseResult = await purchaseCart();
+            if (purchaseResult) {
+                setTicket(purchaseResult);
+            }
+        } catch (error) {
+            console.error('Error on purchase process:', error);
+        }
+    }
+
     return (
         <CartContext.Provider
             value={{
                 cart,
+                fetchCart,
                 setCart,
                 addToCart,
                 removeItemById,
@@ -117,8 +130,8 @@ export const CartProvider = ({ children }) => {
                 totalPrice,
                 setTotalPrice,
                 totalItems,
-                orderId,
-                setOrderId,
+                ticket,
+                buyCart,
             }}
         >
             {children}
